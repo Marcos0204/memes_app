cee17d5cfecb9b19a0d353da08cdb1079be9d26c
Marcos Bastidas <marcosbastidas301@gmail.com>
2021-10-10 17:56:29 -0500

    add-funtion-to-canvas

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-//import html2canvas from 'html2canvas';
+import html2canvas from 'html2canvas';
 import { useState } from 'react';
 import './App.css';
 
@@ -23,13 +23,13 @@ function App() {
 
   const onClickExportar = function (evento) {
 
-    // html2canvas(document.querySelector("#meme")).then(canvas => {
-    //   var img = canvas.toDataURL("image/png");
-    //   var link = document.createElement('a');
-    //   link.download = 'meme.png';
-    //   link.href = img;
-    //   link.click();
-    // });
+    html2canvas(document.querySelector("#meme")).then(canvas => {
+      var img = canvas.toDataURL("image/png");
+      var link = document.createElement('a');
+      link.download = 'meme.png';
+      link.href = img;
+      link.click();
+    });
   }
 
   return (
